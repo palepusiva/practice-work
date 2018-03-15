4f685f0b202a156e157608fa76c7fa81992057e5
palepu siva <siva891989@gmail.com>
2018-03-15 10:00:46 +0530

    FirstCommitProgram

---
+++ src/com/siva/practice/FirstCommit.java
@@ -0,0 +1,12 @@
+package com.siva.practice;
+import org.testng.annotations.Test;
+
+public class FirstCommit {
+
+	@Test
+	public static void print()
+	{
+		System.out.println("Sample Program to Push in Git");
+	}
+	
+}
